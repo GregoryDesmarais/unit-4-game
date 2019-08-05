a77e646c5abf37db593000dda96ad2fef16ad0fc
GDesmarais <46517644+GregoryDesmarais@users.noreply.github.com>
2019-08-05 19:43:03 -0400

    Added obiWan.jpg Added charSelect and charPanels to index.html Added initial styling to charPanels

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,24 @@
 var charStats = {
     lukeSky:{
+        name: "Luke Skywalker",
         baseAtk: 6,
         curAtk: 6,
         hp: 100
     },
     obiWan: {
+        name: "Obi-Wan Kenobi",
         baseAtk: 4,
         curAtk: 4,
         hp: 120
     },
     darthSidious: {
+        name: "Darth Sidious",
         baseAtk: 5,
         curAtk: 5,
         hp: 150
     },
     darthMaul: {
+        name: "Darth Maul",
         baseAtk: 7,
         curAtk: 7,
         hp: 180
