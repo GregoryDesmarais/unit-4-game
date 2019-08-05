4601e78cd7b556bbfdcc42eb468339cffa29a72d
GDesmarais <46517644+GregoryDesmarais@users.noreply.github.com>
2019-08-05 19:10:12 -0400

    Added index.html with base template.  Added links and scripts to CSS and JS. Added character object to game.js.

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+var charStats = {
+    lukeSky:{
+        baseAtk: 6,
+        curAtk: 6,
+        hp: 100
+    },
+    obiWan: {
+        baseAtk: 4,
+        curAtk: 4,
+        hp: 120
+    },
+    darthSidious: {
+        baseAtk: 5,
+        curAtk: 5,
+        hp: 150
+    },
+    darthMaul: {
+        baseAtk: 7,
+        curAtk: 7,
+        hp: 180
+    }
+
+};
+
+$(function(){
+
+});
